708b5c6d8ccaabe7b9191a385ba87dd1c4a5af05
Kuba Odias <agrapho.pl@gmail.com>
2014-06-14 12:29:47 +0200

    Display posts.

--- index.php
+++ index.php
@@ -4,19 +4,18 @@
 
     <div id="header-carousel" class="carousel slide">
     <div class="carousel-inner">
-      <div class="item active">
-        <img src="/propagita.pl/wp-content/uploads/2014/06/budynek_strona_glowna_1.jpg" alt=""></img>
-      </div>
-      <div class="item">
-        <img src="/propagita.pl/wp-content/uploads/2014/06/budynek_strona_glowna_2.jpg" alt=""></img>
-      </div>
-      <div class="item">
-        <img src="/propagita.pl/wp-content/uploads/2014/06/budynek_strona_glowna_3.jpg" alt=""></img>
-      </div>
-      <div class="item">
-        <img src="/propagita.pl/wp-content/uploads/2014/06/budynek_strona_glowna_4.jpg" alt=""></img>
-      </div>
-    </div>
+    <?php $args = array( 'post_type' => 'post',
+                         'posts_per_page' => -1,
+                         'order' => 'ASC' );
+          $posts = get_posts( $args );
+          $is_first_item = true;
+          foreach ( $posts as $post ) {
+             $attachments = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full'); ?>
+             <div class="item <?php if ($is_first_item) { echo active; $is_first_item = false; } ?>">
+               <img src="<?php echo $attachments[0]; ?>" alt=""></img>
+             </div>
+          <?php } ?>
+    </div> <!-- /.carousel-inner -->
 
     <!-- Controls -->
     <a class="left carousel-control" href="#header-carousel" data-slide="prev">
