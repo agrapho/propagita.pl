2bf5126bbbd51a19674ee56d0323849bb8c218a6
Kuba Odias <agrapho.pl@gmail.com>
2014-06-17 18:59:40 +0200

    Text in footer.

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <div class="footer">
 
-  <div class="content">
-    <p>Copyright &copy Agrapho Design | Powered by <a href='http://wordpress.org'>WordPress</a></p>
+  <div class="content container">
+    <h3>Rozwiązania projektowe i poszukiwanie idealnie pasującej formy,<br>które urzeczywistnią Twoją wyobraźnię</h3>
+    <p>Copyright &copy <a target="_blank" href="http://www.agrapho.com.pl">Agrapho Design</a> | Powered by <a href='http://wordpress.org'>WordPress</a></p>
   </div> <!-- /.content -->
  
   <?php wp_footer(); ?>
