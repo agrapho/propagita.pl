dda2d10c0c6e468209a500d5b91133e43ade224a
Kuba Odias <kuba.odias@gmail.com>
2015-10-10 14:08:21 +0200

    Style refactoring.

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
 <div class="footer">
 
   <div class="content container">
-    <h3><?php $current_language = qtrans_getLanguage();
+  <!--  <h3><?php $current_language = qtrans_getLanguage();
               echo qtrans_use($current_language, '[:pl]Rozwiązania projektowe i poszukiwanie idealnie pasującej formy,<br>które urzeczywistnią Twoją wyobraźnię[:en]Project solutions and looking for an ideally matching form,<br>that make your imagination a reality.'); ?>
-    </h3>
-    <p>Copyright &copy <a target="_blank" href="http://www.agrapho.com.pl">Agrapho Design</a> | Powered by <a href='http://wordpress.org'>WordPress</a></p>
+    </h3> -->
   </div> <!-- /.content -->
  
   <?php wp_footer(); ?>
