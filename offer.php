e3f0fe38227f13a9c88b4298f92df18d8b9d2247
Kuba Odias <kuba.odias@gmail.com>
2015-10-17 11:24:14 +0200

    Offer details.

--- offer.php
+++ offer.php
@@ -16,22 +16,50 @@ Template Name: Oferta
                          'post_parent' => get_the_ID(),
                          'order' => 'ASC' );
           $posts = get_posts( $args );
-          $index = 0;
+          $i = 0;
           foreach ( $posts as $post ) {
              $attachments = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full'); ?> 
              <style>
-                .offer .offer-category-<?php echo $index; ?>{
+                .offer .offer-category-<?php echo $i; ?>{
                     background-image: url(<?php echo $attachments[0]; ?>);
                 }
              </style>
              <div class="span2 offer-category-outer">
-               <div class="offer-category offer-category-<?php echo $index; ?> background-cover">
+               <div class="offer-category offer-category-<?php echo $i; ?> background-cover">
                  <div class="vertical-text"><h2><?php echo the_title(); ?></h2></div>
                </div> <!-- /.offer-category -->
              </div> <!-- /.offer-category-outer -->
-          <?php
-             $index++;
-          } wp_reset_query(); ?>
+           <?php
+             $i++;
+
+             # display offer details
+#             $temp_query = $wp_query;
+#             $offer_category = get_category_by_slug($post->post_name);
+#             $args = array( 'post_type' => 'post',
+#                            'posts_per_page' => -1,
+#                            'category' => $offer_category->term_id,
+#                            'order' =>'ASC' );
+#             $offer_details = get_posts( $args );
+#             $j = 0;
+#             foreach ( $offer_details as $offer_detail ) {
+#                if (($j % 2) == 0) {
+#                    echo '<div class="span2 offer-detail-outer">';
+                } ?>
+<!--                    <div class="offer-detail">
+                        <p><?php echo $offer_detail->post_title; ?></p>
+                    </div>
+-->
+          <?php #if (($j % 2) == 1) {
+#                    echo '</div>';
+#                }
+#                $j++;
+#             }
+#
+#             $wp_query = $temp_query;
+#
+#             break; -->
+#          }
+        wp_reset_query(); ?>
     <div class="span2">
     </div>
   </div><!-- /.offer -->
