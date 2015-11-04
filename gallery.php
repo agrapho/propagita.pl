2124889ce2c300db055c97ed551674c89555c949
Kuba Odias <kuba.odias@gmail.com>
2015-11-04 09:44:15 +0100

    Gallery with layout grid.

---
+++ gallery.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Template Name: Portfolio
+*/
+?>
+
+<?php get_header(); ?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/layout-grid/dist/css/layout-grid.min.css" />
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/layout-grid/dist/js/layout-grid.min.js"></script>
+
+<div class="row-fluid background background-cover">
+  <div class="container gallery">
+
+    <div class="lt-container lt-lg-h-10">
+      <div class="lt
+                  lt-lg-x-0 lt-lg-y-0 lt-lg-w-2 lt-lg-h-1">
+        <div class="lt-body">
+            <h3>1</h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-2 lt-lg-y-0 lt-lg-w-1 lt-lg-h-1">
+        <div class="lt-body">
+            <h3>2</h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-3 lt-lg-y-0 lt-lg-w-1 lt-lg-h-2">
+        <div class="lt-body">
+            <h3>3</h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-0 lt-lg-y-1 lt-lg-w-1 lt-lg-h-1">
+        <div class="lt-body">
+            <h3>4</h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-1 lt-lg-y-1 lt-lg-w-2 lt-lg-h-1">
+        <div class="lt-body">
+            <h3>5</h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-0 lt-lg-y-2 lt-lg-w-1 lt-lg-h-2">
+        <div class="lt-body">
+            <h3></h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-1 lt-lg-y-2 lt-lg-w-1 lt-lg-h-1">
+        <div class="lt-body">
+            <h3></h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-2 lt-lg-y-2 lt-lg-w-2 lt-lg-h-1">
+        <div class="lt-body">
+            <h3></h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-1 lt-lg-y-3 lt-lg-w-2 lt-lg-h-1">
+        <div class="lt-body">
+            <h3></h3>
+        </div>
+      </div>
+      <div class="lt
+                  lt-lg-x-3 lt-lg-y-3 lt-lg-w-1 lt-lg-h-1">
+        <div class="lt-body">
+            <h3></h3>
+        </div>
+      </div>
+    </div><!-- /.lt-container -->
+
+  </div><!-- /.gallery -->
+</div><!-- /.row-fluid -->
+
+<?php get_footer(); ?>
