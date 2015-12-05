d8da57dfe19aa7462c53d9f6b0c8d93ea23e0f37
Kuba Odias <kuba.odias@gmail.com>
2015-12-05 14:45:10 +0100

    Gallery updated.

--- gallery.php
+++ gallery.php
@@ -16,110 +16,142 @@ Template Name: Portfolio
 <div class="row-fluid background background-cover">
   <div class="container gallery">
 
-    <div class="lt-container lt-lg-h-10">
+    <div class="lt-container lt-lg-h-14">
       <div class="lt
                   lt-lg-x-0 lt-lg-y-0 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/lonv1fb.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/01-projekt-koncepcyjny.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-2 lt-lg-y-0 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/M-dadak-A1.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/02-aranzacja-wnetrza.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-3 lt-lg-y-0 lt-lg-w-1 lt-lg-h-2">
-        <?php display_gallery_item("wp-content/uploads/2015/11/krzyz2a.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/03-model-3D-projekt-wykonawstwo.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-0 lt-lg-y-1 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/2015-03-24-unimetal-wizytowka.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/04-system-identyfikacji-wizualnej-i-modelowanie-3D.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-1 lt-lg-y-1 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/AL1fb.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/05-projekt-koncepcyjny.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-0 lt-lg-y-2 lt-lg-w-1 lt-lg-h-2">
-        <?php display_gallery_item("wp-content/uploads/2015/11/8-piec.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/06-konkurs-wystawa-model-3D-i-aranzacja.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-1 lt-lg-y-2 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/2015-05-5-TYLICZ-lazienkaB.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/07-aranzacja-wnetrza.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-2 lt-lg-y-2 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/modrzejewska.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/08-koncepcja-rewitalizacji.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-1 lt-lg-y-3 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/w4.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/09-konkurs-wystawa-energetyczne-centrum-nauki-kielecki-park-technologiczny.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-3 lt-lg-y-3 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/g10_0005.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/10-grafika-2D-fototapeta.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-0 lt-lg-y-4 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/11-system-identyfikacji-wizualnej.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-2 lt-lg-y-4 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/dw3fba.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/12-projekt-koncepcyjno-budowlany-dworku.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-0 lt-lg-y-5 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/Open_Up-oferta-szkoleniowa-1.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/13-system-identyfikacji-wizualnej-prezentacyjnej.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-1 lt-lg-y-5 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/Untitled-1.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/14-modelowanie-3D-i-wizualizacja.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-3 lt-lg-y-5 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/Maja-2a.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/15-wizualizacje-projektu-dyplomowego.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-0 lt-lg-y-6 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/2015-02-21-Walczowscy 5A.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/16-koncepcja-elewacji-wizualizacje.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-2 lt-lg-y-6 lt-lg-w-1 lt-lg-h-2">
-        <?php display_gallery_item("wp-content/uploads/2015/11/M1FB.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/17-aranzacja-wnetrza-lazienki.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-3 lt-lg-y-6 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/18-system-identyfikacji-wizualnej.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-0 lt-lg-y-7 lt-lg-w-1 lt-lg-h-2">
-        <?php display_gallery_item("wp-content/uploads/2015/11/salon-fb2.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/19-aranzacja-wnetrza-salonu.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-1 lt-lg-y-7 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/AL3fb.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/20-projekt-koncepcyjny.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-3 lt-lg-y-7 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/allo4bfb.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/21-projekt-model-3D-i-wykonastwo-chrzcielnica.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-1 lt-lg-y-8 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/matejczyk-1.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/22-projekt-budowlany.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-2 lt-lg-y-8 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/Miekinia1aA-OB.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/23-aranzacja-wnetrza-poddasza.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-0 lt-lg-y-9 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/M2FB.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/24-system-identyfikacji-wizualnej-projekt-logo-i-koszulek.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-1 lt-lg-y-9 lt-lg-w-2 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/dw1fb.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/25-model-3D-i-wizualizacja.jpg"); ?>
       </div>
       <div class="lt
                   lt-lg-x-3 lt-lg-y-9 lt-lg-w-1 lt-lg-h-1">
-        <?php display_gallery_item("wp-content/uploads/2015/11/2015-06-01-Kaprin-Dennica-PSJ-przekroj.jpg"); ?>
+        <?php display_gallery_item("wp-content/uploads/2015/12/26-aranzacja-wnetrza-dworku.jpg"); ?>
+      </div>
+      <div class="lt
+                  lt-lg-x-0 lt-lg-y-10 lt-lg-w-1 lt-lg-h-1">
+        <?php display_gallery_item("wp-content/uploads/2015/12/27-projekt-budowlano-wykonawczy.jpg"); ?>
+      </div>
+      <div class="lt
+                  lt-lg-x-1 lt-lg-y-10 lt-lg-w-1 lt-lg-h-2">
+        <?php display_gallery_item("wp-content/uploads/2015/12/28-grafika-2D-i-3D.jpg"); ?>
+      </div>
+      <div class="lt
+                  lt-lg-x-2 lt-lg-y-10 lt-lg-w-2 lt-lg-h-1">
+        <?php display_gallery_item("wp-content/uploads/2015/12/29-projekt-elewacji.jpg"); ?>
+      </div>
+      <div class="lt
+                  lt-lg-x-0 lt-lg-y-11 lt-lg-w-1 lt-lg-h-1">
+        <?php display_gallery_item("wp-content/uploads/2015/12/30-projekt-wnetrza-model-3D-i-wizualizacja.jpg"); ?>
+      </div>
+      <div class="lt
+                  lt-lg-x-2 lt-lg-y-11 lt-lg-w-1 lt-lg-h-1">
+        <?php display_gallery_item("wp-content/uploads/2015/12/31-aranzacja-wnetrza.jpg"); ?>
+      </div>
+      <div class="lt
+                  lt-lg-x-3 lt-lg-y-11 lt-lg-w-1 lt-lg-h-2">
+        <?php display_gallery_item("wp-content/uploads/2015/12/32-grafika-2D-i-3D-projekt-okladki.jpg"); ?>
+      </div>
+      <div class="lt
+                  lt-lg-x-0 lt-lg-y-12 lt-lg-w-2 lt-lg-h-1">
+        <?php display_gallery_item("wp-content/uploads/2015/12/33-projekt-i-wizualizacje-wnetrza-sypialni.jpg"); ?>
+      </div>
+      <div class="lt
+                  lt-lg-x-2 lt-lg-y-12 lt-lg-w-1 lt-lg-h-1">
+        <?php display_gallery_item("wp-content/uploads/2015/12/34-projekt-koncepcyjno-budowlany.jpg"); ?>
       </div>
     </div><!-- /.lt-container -->
 
