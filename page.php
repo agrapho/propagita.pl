f6bc06e737fc723e1e2bbdf4c3fe8efb7d139730
Kuba Odias <kuba.odias@gmail.com>
2015-12-12 15:10:03 +0100

    Content background fixes.

--- page.php
+++ page.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
-<div id="content" class="container background background-cover">
-  <?php $temp_query = $wp_query;
+<div id="content-container" class="container background background-cover">
+  <div id="content" class="container">
+    <?php $temp_query = $wp_query;
         while ( have_posts() ) { the_post();
             echo the_content(); 
         }
         $wp_query = $temp_query; ?>
-</div><!-- /#content -->
+  </div><!-- /#content -->
+</div><!-- /#content-container -->
 
 <?php get_footer(); ?>
